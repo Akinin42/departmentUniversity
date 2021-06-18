c82643ee4b10d5c38d09fb104ba717f0264effc5
Igor Akinin <akinin32167@gmail.com>
2021-06-18 19:53:48 +0700

    add classroom controller

---
+++ DepartmentUniversity/src/main/java/org/university/controller/ClassroomController.java
@@ -0,0 +1,39 @@
+package org.university.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.university.dto.CourseDto;
+import org.university.service.ClassroomService;
+
+@Controller
+@RequestMapping("/classrooms")
+public class ClassroomController {
+    
+    @Autowired
+    private ClassroomService classroomService;
+    
+    @GetMapping()
+    public String getAll(Model model) {
+        model.addAttribute("classroom", new ClassroomDto());
+        model.addAttribute("classrooms", classroomService.findAllClassrooms());
+        return "classrooms";
+    }
+    
+    @PostMapping()
+    public String add(@ModelAttribute("classroom") ClassroomDto classroom) {        
+        classroomService.addCourse(classroom);  
+        return "redirect:/classrooms";
+    }
+    
+    @PostMapping("/delete")
+    public String delete(@ModelAttribute("classroom") ClassroomDto classroom) {        
+//        classroomService.delete(classroom);
+        return "redirect:/classrooms";
+    }
+
+}
