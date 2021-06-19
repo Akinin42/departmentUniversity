d4d86a44ecc1cf1eca543b58055fa6d8ff345984
Igor Akinin <akinin32167@gmail.com>
2021-06-19 17:11:56 +0700

    add moving on student list

--- DepartmentUniversity/src/main/java/org/university/controller/StudentController.java
+++ DepartmentUniversity/src/main/java/org/university/controller/StudentController.java
@@ -5,22 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.university.dto.StudentDto;
 import org.university.service.StudentService;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
-    
+
     @Autowired
     private StudentService studentService;
-    
+    private int number;
+
     @GetMapping()
-    public String getFirstTenStudents(Model model) {
-        int number = 0;
-        model.addAttribute("number", number);
+    public String getStudents(Model model) {        
         model.addAttribute("student", new StudentDto());
-        model.addAttribute("students", studentService.findNumberOfUsers(10, number));
+        model.addAttribute("students", studentService.findNumberOfUsers(5, number));
+        return "students";
+    }
+
+    @GetMapping("/other")
+    public String getOtherStudents(@RequestParam("number")int inputNumber, Model model) {
+        number += inputNumber;        
+        model.addAttribute("student", new StudentDto());
+        model.addAttribute("students", studentService.findNumberOfUsers(5, number));
         return "students";
     }
 
